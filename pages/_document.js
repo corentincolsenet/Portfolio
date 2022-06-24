261a32c1292f2881a5d510f5f61f9e6466e54966
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-06-24 18:47:15 +0200

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,32 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <NextScript
-                        strategy="afterInteractive"
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    {/* <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+                    />
+                    <script
                         dangerouslySetInnerHTML={{
-                            __html: `
-                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer',${process.env.GOOGLE_TAG_MANAGER});
-                            `,
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
+                        }}
+                    /> */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER}');
+                    `,
                         }}
                     />
                 </Head>
